c8dde5567bdddfd4f1aa73923f0dedcc8d5b2362
Andre Steudel <andre.steudel@yellowmap.de>
2021-08-24 15:17:08 +0200

    prepare

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
 import { readFileSync } from "fs";
 
 const file = readFileSync("./src/input.txt").toString();
-console.log(file);
+const example = `cpy 41 a
+inc a
+inc a
+dec a
+jnz a 2
+dec a`;
+console.log(example);
